3b366c2a745853787b50ebec1b4ab2a6a178cc21
urvashi1206 <44489378+urvashi1206@users.noreply.github.com>
2024-02-22 15:09:53 -0500

    Player Movement

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,12 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Player : MonoBehaviour
 {
 
     [SerializeField] float mouseSensitivity = 3f;
-    [SerializeField] float movementSpeed = 5f;
+    [SerializeField] float movementSpeed = 20f;
     [SerializeField] Transform cameraTransform;
 
     Vector2 look;
@@ -32,16 +33,10 @@ public class Player : MonoBehaviour
     //Player movement
     void UpdateMovement()
     {
-        //var x = Input.GetAxis("Horizontal");
-        var y = Input.GetAxis("Vertical");
-
-        var input = new Vector3();
-        input += transform.forward * y;
-        //input+=transform.right * x;
-        input = Vector3.ClampMagnitude(input, 1f);
-
-        //transform.Translate(input * movementSpeed * Time.deltaTime, Space.World);
-        controller.Move(input * movementSpeed * Time.deltaTime);
+        if(Input.GetKey(KeyCode.W))
+        {
+            transform.position += Vector3.right * movementSpeed * Time.deltaTime;
+        }   
     }
 
     //Mouse Look at x and y directions
@@ -50,7 +45,7 @@ public class Player : MonoBehaviour
         look.x += Input.GetAxis("Mouse X") * mouseSensitivity;
         look.y += Input.GetAxis("Mouse Y") * mouseSensitivity;
 
-        look.x = Mathf.Clamp(look.x, -89f, 89f);
+        look.x = Mathf.Clamp(look.x, -45f, 240f);
         look.y = Mathf.Clamp(look.y, -89f, 89f);
 
         cameraTransform.localRotation = Quaternion.Euler(-look.y, 0, 0);
